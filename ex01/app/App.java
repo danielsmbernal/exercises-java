e428255f8a105053f715a72bc4b35f6cb9f48a80
Daniel Bernal <danielbernal85@gmail.com>
2022-04-29 20:00:02 -0300

    Java exercises for practising

---
+++ ex01/app/App.java
@@ -0,0 +1,18 @@
+package ex01.app;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+class App {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+
+        
+
+
+        input.close();
+    }
+
+}
